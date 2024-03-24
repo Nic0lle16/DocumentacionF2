e281a2d04e88f8330e60bf7b7a3dc272d41ace16
Nic0lle16 <nicolleabrilpalomino@gmail.com>
2024-03-24 01:22:37 -0500

    primer commit

--- classic/docusaurus.config.ts
+++ classic/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://NicollePalomino.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'DocumentacionF2',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'NicollePalomino', // Usually your GitHub org/user name.
+  projectName: 'DocumentacionF2', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
